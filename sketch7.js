661499ef23e0371128053ee9e6046629c3c50eeb
anshuchil23 <55515346+anshuchil23@users.noreply.github.com>
2020-04-15 15:31:13 -0400

    Update sketch7.js

--- sketch7.js
+++ sketch7.js
@@ -3,42 +3,29 @@ let mic, fft;
 
 var balls = [];
 
-function setup(){
+function setup() {
   createCanvas(640, 480);
   pixelDensity(1);
   video = createCapture(VIDEO);
-    mic = new p5.AudioIn();
-video.hide();
-   mic.start();
+  mic = new p5.AudioIn();
+  video.hide();
+  mic.start();
   fft = new p5.FFT();
   fft.setInput(mic);
 }
 
 function draw() {
   // background(0,5);
-  
-  
-  
-        // let vol = mic.getLevel();
 
-      // let h = map(vol, 0, 1, 10, 100);
-        let spectrum = fft.analyze();
-  
-    // fill(c);
-  // noStroke();
+  let spectrum = fft.analyze();
 
-  for (i = 0; i < spectrum.length; i+=20) {
-      let a = map(spectrum[i], 0, 255, height, -150);
-      let c = video.get(i, a);
+
+  for (i = 0; i < spectrum.length; i += 20) {
+    let a = map(spectrum[i], 0, 255, height, -150);
+    let c = video.get(i, a);
     stroke(c);
     noFill();
-    rect(i, a, random(30),10);
+    rect(i, a, random(30), 10);
   }
-  // ellipse(x1+h,y1+h,h,h);
-  // stroke(c);
-  // strokeWeight(h);
-  // point(x1, y1);
-  //   }
-  // }
-// image(capture, 0, 0, width,height); 
+
 }
